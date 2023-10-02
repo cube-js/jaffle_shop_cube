0d8cdad55087f2c0c8e256368f7b40e8ce62447c
igor <igor@cube.dev>
2023-10-02 14:57:21 +0000

    Update to new API

--- cube/cube.py
+++ cube/cube.py
@@ -1,3 +1,46 @@
-from cube.conf import config
+from cube import config
 
 config.schema_path = 'model'
+
+config.base_path = '/cube-api'
+
+# web_sockets_base_path
+# logger
+# driver_factory
+# contextToApiScopes
+# contextToAppId
+# contextToOrchestratorId
+# repositoryFactory
+# checkAuth
+# checkSqlAuth
+# canSwitchSqlUser
+# queryRewrite
+# preAggregationsSchema
+# schemaVersion
+# scheduledRefreshTimer
+# scheduledRefreshTimeZones
+# scheduledRefreshContexts
+# extendContext
+# compilerCacheSize
+# maxCompilerCacheKeepAlive
+# updateCompilerCacheKeepAlive
+# allowUngroupedWithoutPrimaryKey
+# telemetry
+# http.cors
+# jwt
+# jwkUrl
+# key
+# algorithms
+# issuer
+# audience
+# subject
+# claimsNamespace
+# cacheAndQueueDriver
+# orchestratorOptions
+# allowJsDuplicatePropsInSchema
+# initApp
+# processSubscriptionsInterval
+# QueueOptions
+# RequestContext
+# securityContext
+# SchemaFileRepository
