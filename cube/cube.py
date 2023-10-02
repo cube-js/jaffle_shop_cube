ff0044820dba5234ea44beb3f4b2b1c7d66fb301
igor <igor@cube.dev>
2023-10-02 17:50:20 +0000

    Bits of config

--- cube/cube.py
+++ cube/cube.py
@@ -1,20 +1,55 @@
 from cube import config
 
-config.schema_path = 'model'
+# config.schema_path = 'model'
 
-config.base_path = '/cube-api'
+# config.base_path = '/cube-api'
 
-# web_sockets_base_path
-# logger
-# driver_factory
-# contextToApiScopes
+# Not tested
+# config.web_sockets_base_path = '/cube-web-sockets'
+
+# @config('logger')
+# def logger(message: str, params: dict) -> None:
+#   print(f"{message}: {params}")
+
+# @config('driver_factory')
+# def driver_factory(ctx: dict) -> None:
+#   context = ctx['securityContext']
+#   data_source = ctx['dataSource']
+#   print(context)
+#   print(data_source)
+ 
+#   return {
+#     'type': 'postgres',
+#     'host': 'demo-db-examples.cube.dev',
+#     'user': 'cube',
+#     'password': '12345',
+#     'database': 'ecom'
+#   }
+ 
+# @config('context_to_api_scopes')
+# def context_to_api_scopes(context: dict, default_scopes: list[str]) -> list[str]:
+#   return ['meta', 'data', 'graphql']
+
 # contextToAppId
 # contextToOrchestratorId
 # repositoryFactory
 # checkAuth
 # checkSqlAuth
 # canSwitchSqlUser
-# queryRewrite
+
+# @config('query_rewrite')
+# def query_rewrite(query: dict, ctx: dict) -> dict:
+#   query['measures'] = ['orders.count']
+  
+#   if 'min_count' in ctx['securityContext']:
+#     query['filters'] = [{
+#       'member': 'orders.count',
+#       'operator': 'gt',
+#       'values': [ctx['securityContext']['min_count']]
+#     }]
+  
+#   return query
+
 # preAggregationsSchema
 # schemaVersion
 # scheduledRefreshTimer
