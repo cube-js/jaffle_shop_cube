03dcdb1422ce7e7be665a59fe0de9ef34e30b629
igor <igor@cube.dev>
2023-10-11 15:12:24 +0000

    Update

--- cube/model/globals.py
+++ cube/model/globals.py
@@ -3,9 +3,12 @@ from cube_dbt import Dbt
 
 template = TemplateContext()
 
-manifest_url = 'https://cube-dbt-integration.s3.amazonaws.com/manifest-jaffle-shop-cube.json'
+manifest_url = 'https://cube-dbt-integration.s3.amazonaws.com/manifest.json'
 
 dbt = Dbt.from_url(manifest_url).filter(paths=['marts/'])
+
+for model in dbt.models:
+  print(model)
 
 @template.function('dbt_model')
 def dbt_model(name):
